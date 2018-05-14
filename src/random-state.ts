a1240c8d06122b6a69c8d0ce050816a58349c026
Michal Szewczak <michal@szewczak.email>
2018-05-15 00:00:00 +0100

    serve files statically

--- src/random-state.ts
+++ src/random-state.ts
@@ -1,5 +1,5 @@
 import Faker from 'faker';
-import { range } from 'lodash';
+import { range, random } from 'lodash';
 import { IUser, INote } from '@/app-state';
 
 const DUMMY_PICTURE_COUNT = 3;
@@ -8,25 +8,12 @@ const DUMMY_AVATAR_COUNT = 3;
 const POST_COUNT = 30;
 const USER_COUNT = 5;
 
-/* From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
-function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; // The maximum is exclusive and the minimum is inclusive
-}
-
 function getRandomImageUrl() {
-  return require(`./assets/dummy-data/pictures/${getRandomInt(
-    1,
-    DUMMY_PICTURE_COUNT + 1,
-  )}.png`);
+  return `/dummy-data/pictures/${random(1, DUMMY_PICTURE_COUNT)}.png`;
 }
 
 function getRandomAvatarUrl() {
-  return require(`./assets/dummy-data/avatars/${getRandomInt(
-    1,
-    DUMMY_AVATAR_COUNT + 1,
-  )}.png`);
+  return `/dummy-data/avatars/${random(1, DUMMY_AVATAR_COUNT)}.png`;
 }
 
 function getRandomItem<T>(items: T[]) {
@@ -52,13 +39,13 @@ export function getRandomNote(userPool: IUser[], nestingLevel = 0): INote {
 
   return {
     imageUrl: Math.random() < 0.5 ? getRandomImageUrl() : null,
-    text: Faker.lorem.sentences(getRandomInt(1, 10)),
+    text: Faker.lorem.sentences(random(1, 10)),
     time: Faker.date.between(now, fiveDaysAgo),
     author: getRandomUser(),
     children:
       nestingLevel === 0
         ? null
-        : range(Math.random() < 0.7 ? 0 : getRandomInt(1, 5)).map(() =>
+        : range(Math.random() < 0.7 ? 0 : random(1, 5)).map(() =>
             getRandomNote(userPool, nestingLevel - 1),
           ),
   };
