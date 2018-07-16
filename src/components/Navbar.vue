835f723ade727de8437100c917bf8f85d9fa6850
X1Zeth2X <zethiscool11@gmail.com>
2018-07-16 22:51:30 +0000

    Added a simple Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar is-info" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="#">
+        <img src="@/assets/Brand.png" alt="Konishi Logo" width="130">
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        </a>
+    </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
